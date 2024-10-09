a6bacec9a9c1c1eda80d93880e8c46e77173b659
andreespirela <andreespirela@outlook.com>
2024-10-09 02:05:53 -0400

    chore: Debugging

--- crates/data/src/shard/shards/data_shard/shard.rs
+++ crates/data/src/shard/shards/data_shard/shard.rs
@@ -28,6 +28,11 @@ impl DataShard {
         let item_pos =
             { header_read.get_offset_value_from_offset_header(offset_position_in_header) };
 
+        println!(
+            "Offset {} has a value of {:?}",
+            offset_position_in_header, item_pos
+        );
+
         match item_pos {
             None => Err(ShardErrors::UnknownOffset),
             Some(start_pos) => {
@@ -107,6 +112,10 @@ impl Shard<DataShardConfig> for DataShard {
     fn read_item_from_index(&self, index: usize) -> Result<Vec<u8>, ShardErrors> {
         let header = self.header.read();
         let offset_pos_in_header = header.get_offset_pos_by_index(index);
+        println!(
+            "Index {} has an offset of {:?}",
+            index, offset_pos_in_header
+        );
         match offset_pos_in_header {
             None => Err(ShardErrors::UnknownOffset),
             Some(pos_in_header) => self.read_item(pos_in_header),
