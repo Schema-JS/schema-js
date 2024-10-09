1dc1856967003d3ee19704fefd0ff5cf31f42932
andreespirela <andreespirela@outlook.com>
2024-10-09 00:49:24 -0400

    chore: more debugging

--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -6,5 +6,5 @@ pub mod temp_offset_types;
 pub mod utils;
 
 // https://doc.rust-lang.org/std/mem/fn.size_of.html
-pub const U64_SIZE: usize = 8;
-pub const I64_SIZE: usize = 8;
+pub const U64_SIZE: usize = size_of::<u64>();
+pub const I64_SIZE: usize = size_of::<i64>();
